6f6775a942da9fc44aab2928b01fb528de104384
Alex <roden20030823@mail.ru>
2022-12-09 15:55:38 +0200

    vue

---
+++ vite-project/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="sass">
+body
+  background-color: #eaaa00
+</style>
---
+++ vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+  const data = reactive({symbol: ""})
+  function addText(value){
+    data.symbol = value
+  }
+
+</script>
+
+<template>
+  <input v-model="myInput" @keypress.enter="addText(myInput)">
+  <p v-if="data.symbol">{{data.symbol}}</p>
+  <p v-else>Текста нет</p>
+</template>
+
+<style scoped lang="sass">
+
+input
+  font-family: Arial, sans-serif
+  background-color: #eaaa00
+  border: 3px solid #FFFFFF
+  font-size: 18px
+  outline: none
+  padding: 10px
+  color: #FFFFFF
+p
+  font-family: Arial, sans-serif
+  font-size: 18px
+  color: #FFFFFF
+
+
+</style>
